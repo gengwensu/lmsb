5e5ab1c115911c0a09410bc4c99b06a113817ef7
GarySu <Gary.Su@hearst.com>
2019-12-13 14:57:18 -0500

    Get rid of constructor in EventDispatcher

--- src/main/java/microservices/book/multiplication/event/EventDispatcher.java
+++ src/main/java/microservices/book/multiplication/event/EventDispatcher.java
@@ -1,5 +1,6 @@
 package microservices.book.multiplication.event;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,21 +9,22 @@ import org.springframework.stereotype.Component;
  * Handles the communication with the Event Bus.
  */
 @Component
+@RequiredArgsConstructor
 public class EventDispatcher {
 
     private RabbitTemplate rabbitTemplate;
-
+    @Value("${multiplication.exchange}")
     private String multiplicationExchange;
-
+    @Value("${multiplication.solved.key}")
     private String multiplicationSolvedRoutingKey;
 
-    EventDispatcher(final RabbitTemplate rabbitTemplate,
+    /*EventDispatcher(final RabbitTemplate rabbitTemplate,
                     @Value("${multiplication.exchange}") final String multiplicationExchange,
                     @Value("${multiplication.solved.key}") final String multiplicationSolvedRoutingKey) {
         this.rabbitTemplate = rabbitTemplate;
         this.multiplicationExchange = multiplicationExchange;
         this.multiplicationSolvedRoutingKey = multiplicationSolvedRoutingKey;
-    }
+    }*/
 
     public void send(final MultiplicationSolvedEvent multiplicationSolvedEvent) {
         rabbitTemplate.convertAndSend(multiplicationExchange, multiplicationSolvedRoutingKey, multiplicationSolvedEvent);
